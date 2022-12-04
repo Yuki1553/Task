fd72359203cf37a4b13acb420f660bf54b88e468
Yuki <x1e6yhek@icloud.com>
2022-12-04 13:24:57 +0300

    test

---
+++ task_2.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.WriteLine("Enter first numbers: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second numbers: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter third numbers: ");
+int c = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
